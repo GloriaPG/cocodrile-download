910be10dce3e62ea85e605f0a7eb8f22d383ee68
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2017-03-07 17:08:46 -0600

    init

---
+++ cocodrile.py
@@ -0,0 +1,28 @@
+import urllib2
+
+source_plugin = "http://sf-c01.sentinel.la:5580/plugins/download/"
+name_plugin = "test2-1.4"
+extension = "zip"
+url = source_plugin + name_plugin + "." + extension
+
+file_name = url.split('/')[-1]
+u = urllib2.urlopen(url)
+f = open(file_name, 'wb')
+meta = u.info()
+file_size = int(meta.getheaders("Content-Length")[0])
+print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+file_size_dl = 0
+block_sz = 8192
+while True:
+    buffer = u.read(block_sz)
+    if not buffer:
+        break
+
+    file_size_dl += len(buffer)
+    f.write(buffer)
+    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status = status + chr(8)*(len(status)+1)
+    print status,
+
+f.close()
